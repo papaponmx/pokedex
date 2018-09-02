edffd85cae53656f64446069a04bded42f567ccb
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-09-02 18:35:20 -0500

    Adjusted App component test

--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Route } from 'react-router-dom';
 
-import Header from 'components/Header';
+import Navbar from 'containers/Navbar';
 import Footer from 'components/Footer';
 import App from '../index';
 
 describe('<App />', () => {
-  it('should render the header', () => {
+  it('should render the Navbar', () => {
     const renderedComponent = shallow(<App />);
-    expect(renderedComponent.find(Header).length).toBe(1);
+    expect(renderedComponent.find(Navbar).length).toBe(1);
   });
 
   it('should render some routes', () => {
